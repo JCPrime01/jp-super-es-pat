e8f30c4b19c8ac8362b776bcd806334ade710d87
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-16 11:46:06 -0300

    chore: update shareCode to J11SGKM74GW

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zy1z1vkz?shareCode=6YQLSZ9F3A3&afp_campaign=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zy1z1vkz?shareCode=J11SGKM74GW&afp_campaign=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
