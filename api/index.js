2fb0ff597627323e70464cf3ee558a0ce60d4b3b
JCPrime01 <vanzokiller@gmail.com>
2026-06-16 10:53:13 -0300

    chore: shareCode MHAYJHNA7UX -> UMCQDQZ2YLT (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7053&shareCode=MHAYJHNA7UX&afp1_campaign=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7053&shareCode=UMCQDQZ2YLT&afp1_campaign=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
