1ffa7906fed4023eaa2294b9e8cc204298484834
JCPrime01 <vanzokiller@gmail.com>
2026-06-19 12:26:21 -0300

    chore: shareCode HAACPWKSD5E -> 1UMZY7EMPKB (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7053&shareCode=HAACPWKSD5E&afp1_campaign=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7053&shareCode=1UMZY7EMPKB&afp1_campaign=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
